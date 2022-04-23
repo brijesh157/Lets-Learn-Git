96139d78781a83991640dfc51e8684165d40d269
Brijesh Anand <brijeshujcbm@gmail.com>
2022-04-23 20:16:31 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c=30;
+        int d=11;
+        int e=26;
+        System.out.println(a+b+c+d+e);
+    }
+}
